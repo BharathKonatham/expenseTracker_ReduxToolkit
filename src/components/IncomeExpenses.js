b10ae79b6210f6f760fe8554e6d03b4d8c005349
bharath konatham <bharathreddyk121@gmail.com>
2023-11-08 14:53:35 -0500

    added warning message

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -3,16 +3,22 @@ import { GlobalContext } from '../context/GlobalState'
 export const IncomeExpenses = () => {
   const {transactions} = useContext(GlobalContext)
   let amounts = transactions.map(transaction=>transaction.amount)
+  let Income = amounts.filter((value)=>value>0).reduce((total,amount)=>total+amount,0).toFixed(2)
+  let Expense = Math.abs(amounts.filter((value)=>value<0).reduce((total,amount)=>total+amount,0)).toFixed(2)
   return (
-    <div className='inc-exp-container'>
+    <>
+     <div className='inc-exp-container'>
         <div>
             <h4>Income</h4>
-            <p  className='money plus'>${amounts.filter((value)=>value>0).reduce((total,amount)=>total+amount,0).toFixed(2)}</p>
+            <p  className='money plus'>${Income}</p>
         </div>
         <div>
             <h4>Expense</h4>
-            <p className='money minus'>${Math.abs(amounts.filter((value)=>value<0).reduce((total,amount)=>total+amount,0)).toFixed(2)}</p>
+            <p className='money minus'>${Expense}</p>
         </div>
     </div>
+    { Expense > Income ? <h5 style={{color:'red'}}>Expenses are greater than Income</h5>:'' }
+    </>
+   
   )
 }
